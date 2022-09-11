12065ab38e63162e913001a41082e39f35e462dd
Wok <woctezuma@users.noreply.github.com>
2022-09-11 12:48:07 +0200

    Add utils to load the dict: pageSlug -> sandboxId

--- src/data_utils.py
+++ src/data_utils.py
@@ -3,6 +3,7 @@ from src.json_utils import load_json
 DATA_FOLDER_NAME = 'data'
 ACHIEVEMENT_FNAME = 'achievements.json'
 GAME_RATING_FNAME = 'game_ratings.json'
+SANDBOX_IDS_FNAME = 'sandbox_ids.json'
 
 
 def load_achievements():
@@ -11,3 +12,7 @@ def load_achievements():
 
 def load_game_ratings():
     return load_json(f"{DATA_FOLDER_NAME}/{GAME_RATING_FNAME}")
+
+
+def load_sandbox_ids_dict():
+    return load_json(f"{DATA_FOLDER_NAME}/{SANDBOX_IDS_FNAME}")
