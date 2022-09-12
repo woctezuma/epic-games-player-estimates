c80e6a18c5eb2a94c8c864a464e06e98ef5aab15
Wok <woctezuma@users.noreply.github.com>
2022-09-12 17:59:24 +0200

    Rely on data filtering utils in the script

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
-from src.achievement_filters import has_valid_achievements
+from src.data_filters import remove_broken_achievements, intersect_sandbox_ids
 from src.data_utils import load_achievements, load_game_ratings, load_sandbox_ids_dict
 from src.json_utils import save_json
-
-
-def intersect_sandbox_ids(achievement_data, game_rating_data):
-    sandbox_ids = set(achievement_data.keys())
-    print(len(sandbox_ids))
-
-    sandbox_ids = [id for id in sandbox_ids if has_valid_achievements(achievement_data[id])]
-    print(len(sandbox_ids))
-
-    sandbox_ids = set(sandbox_ids).intersection(game_rating_data.keys())
-    print(len(sandbox_ids))
-
-    return sandbox_ids
 
 
 def main():
@@ -21,6 +8,7 @@ def main():
     game_rating_data = load_game_ratings()
     sandbox_ids_dict = load_sandbox_ids_dict()
 
+    achievement_data = remove_broken_achievements(achievement_data)
     sandbox_ids = intersect_sandbox_ids(achievement_data, game_rating_data)
 
     trimmed_sandbox_ids = {k: v for k, v in sandbox_ids_dict.items() if v in sandbox_ids}
