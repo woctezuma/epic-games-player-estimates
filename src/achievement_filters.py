f52109a8556aa5f59aef9e761dc1f56ae6a7476e
Wok <woctezuma@users.noreply.github.com>
2022-09-14 22:40:17 +0200

    Minor refactoring: remove redundant function

--- src/achievement_filters.py
+++ src/achievement_filters.py
@@ -2,10 +2,6 @@ def list_all_unlock_percentages(achievement_data):
     return [e['achievement']['rarity']['percent'] for e in achievement_data['achievements']]
 
 
-def unlocks_achievements(product_data):
+def has_valid_achievements(product_data):
     rarity_list = list_all_unlock_percentages(product_data)
     return len(rarity_list) > 0 and max(rarity_list) > 0
-
-
-def has_valid_achievements(product_data):
-    return unlocks_achievements(product_data)
