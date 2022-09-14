7095b813b125b87883eb74b498585c725b52d5a8
Wok <woctezuma@users.noreply.github.com>
2022-09-14 22:41:16 +0200

    Minor refactoring: rename function argument for consistency

--- src/achievement_filters.py
+++ src/achievement_filters.py
@@ -2,6 +2,6 @@ def list_all_unlock_percentages(achievement_data):
     return [e['achievement']['rarity']['percent'] for e in achievement_data['achievements']]
 
 
-def has_valid_achievements(product_data):
-    rarity_list = list_all_unlock_percentages(product_data)
+def has_valid_achievements(achievement_data):
+    rarity_list = list_all_unlock_percentages(achievement_data)
     return len(rarity_list) > 0 and max(rarity_list) > 0
